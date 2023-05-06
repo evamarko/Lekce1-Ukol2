303d0cbdb3e9935ee929a96f8375b358069f4d8e
Eva Marko <valentova.eva92@gmail.com>
2023-05-06 16:53:30 +1200

    Dokončení úkolu
    Vytvořeny proměnné včetně hodnot a výpis na obrazovku

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String name = "Libor";
+        String surname = "Novotný";
+        int numberOfContracts = 28;
+        double carrotSoldInTons = 62;
+        String city = "Turnov";
+        String carSpz = "3B33333";
+        double vehicleConsumption = 7.9;
+        String ipAddress = "192.168.48.39";
+
+        double averageCarrotSoldPerContract = carrotSoldInTons/numberOfContracts;
+        System.out.println("Průměrné množství prodané mrkve na jednu smlouvu: " + averageCarrotSoldPerContract + " tun.");
     }
 }
